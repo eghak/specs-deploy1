3344bdcfa0f3d5f7e331da2c0e143be28a876d51
Egha Kusuma <eghakusuma1@gmail.com>
2023-08-30 12:52:26 -0700

    update

--- server/index.js
+++ server/index.js
@@ -1,5 +1,12 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+
+const { SERVER_PORT } = process.env;
+const { sequelize } = require("./util/database");
+const { seedDatabase } = require("./util/seed");
+
+const { Text } = require("./models/Text");
 
 const app = express();
 
@@ -7,7 +14,16 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/api/get", (req, res) => {
-  res.status(200).send("It works!");
+  Text.findAll().then((texts) => {
+    console.log(texts);
+    res.status(200).send(texts);
+  });
 });
 
-app.listen(4050, () => console.log(`Jammin on 4050`));
+sequelize
+  .sync()
+  // sequelize.sync({ force: true }).then(() => seedDatabase())
+  .then(() => {
+    app.listen(SERVER_PORT, () => console.log(`Jammin on ${SERVER_PORT}`));
+  })
+  .catch((err) => console.log(err));
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import axios from "axios";
 
 function Header() {
-  const [text, setText] = useState("");
+  const [text, setText] = useState([]);
 
   function getText() {
     axios.get("http://localhost:4050/api/get").then((res) => {
@@ -15,7 +15,9 @@ function Header() {
     <div>
       <h1>Header</h1>
       <button onClick={getText}>Get Text</button>
-      <h1 style={{color: "yellow"}} >{text}</h1>
+      {text.map((item) => {
+        return <h1 style={{ color: "yellow" }}>{item.text}</h1>;
+      })}
     </div>
   );
 }
